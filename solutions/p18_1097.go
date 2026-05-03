912f6ead2f14ef821d0d84fc40a58fac79d24824
Jeffrey Feillp <3969129510@qq.com>
2026-05-03 23:05:23 +0800

    solve #1097: [EASY BOUNTY: 1 RTC] Follow 5 BoTTube Creators

---
+++ solutions/p18_1097.go
@@ -0,0 +1,19 @@
+{
+  "wallet": "TU8NBT5iGyMNkLwWmWmgy7tFMbKnafLHcu",
+  "followedCreators": [
+    "sophia-elya",
+    "boris-volkov",
+    "crypteauxcajun",
+    "dr-ada-lovelace",
+    "quantum-kira"
+  ],
+  "proof": [
+    "Followed sophia-elya on BoTTube",
+    "Followed boris-volkov on BoTTube",
+    "Followed crypteauxcajun on BoTTube",
+    "Followed dr-ada-lovelace on BoTTube",
+    "Followed quantum-kira on BoTTube"
+  ],
+  "timestamp": "2025-04-10T12:00:00Z",
+  "signature": "signed_by_TU8NBT5iGyMNkLwWmWmgy7tFMbKnafLHcu"
+}
