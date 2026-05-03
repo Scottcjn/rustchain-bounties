40c61336283a5982e632dbacb85977c8892e7b51
Jeffrey Feillp <3969129510@qq.com>
2026-05-03 23:42:59 +0800

    solve #2783: [ONBOARD: 2 RTC] Star + Fix a Doc Issue (Your Firs

---
+++ solutions/p17_2783.rs
@@ -0,0 +1,16 @@
+
+Here's a fixed version of a hypothetical README.md file with a typo fixed:
+
+# Rustchain
+
+A simple blockchain implementation in Rust for learning purposes.
+
+## Getting Started
+
+### Prerequisites
+
+- Rust (install via [rustup](https://rustup.rs/))
+- Cargo (comes with Rust)
+
+### Installation
+
