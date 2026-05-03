10cecce405bcf2884627e23cf81318dbb06e084b
Jeffrey Feillp <3969129510@qq.com>
2026-05-03 23:52:45 +0800

    solve #2783: [ONBOARD: 2 RTC] Star + Fix a Doc Issue (Your Firs

---
+++ solutions/p17_2783.rs
@@ -0,0 +1,21 @@
+# Rustchain
+
+A simple blockchain implementation in Rust.
+
+## Features
+
+- Proof of Work consensus
+- Transaction validation
+- Block mining
+- P2P networking
+- Wallet management
+
+## Getting Started
+
+### Prerequisites
+
+- Rust (stable, 1.56+)
+- Cargo
+
+### Installation
+
