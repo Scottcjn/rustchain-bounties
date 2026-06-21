b03c0f14566f4f2041efaba555fb0da139ace640
Kartavya Dikshit <kartavyaniraj.dikshit2021@vitstudent.ac.in>
2026-06-21 07:18:59 +0200

    fix: add SPDX header to create_pr.py

--- create_pr.py
+++ create_pr.py
@@ -1,3 +1,4 @@
+# SPDX-License-Identifier: MIT
 import urllib.request, json, os, ssl, shutil, subprocess
 ctx = ssl.create_default_context(); ctx.check_hostname = False; ctx.verify_mode = ssl.CERT_NONE
 token = os.environ.get("GITHUB_TOKEN")
